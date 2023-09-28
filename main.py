0ad769548be0971b96edfb802733be4cde406d51
Łukasz Lewandowski <elukaszle@tutanota.com>
2023-09-28 11:14:53 +0200

    Mastermind Flask Final

--- main.py
+++ main.py
@@ -2,21 +2,42 @@ from flask import Flask, render_template, request, redirect, url_for, session
 import mastermind_lib
 
 app = Flask(__name__)
-app.secret_key = 'FJ390384230HG0ESFH0EWRH308RH405438'
+app.secret_key = "FJ390384230HG0ESFH0EWRH308RH405438"
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def mastermind():
-    if 'secret' not in session:
-        session['secret'] = mastermind_lib.code()
-        session['attempts'] = 0
-        session['feedback'] = []
+    if "secret" not in session:
+        session["secret"] = mastermind_lib.code()
+        session["attempts"] = 0
+        session["feedback"] = []
 
-    if request.method == 'POST':
-        guess = list(request.form['guess'])
-        feedback = mastermind_lib.check_code(session['secret'], guess)
-        session['feedback'].append({'guess': guess, 'feedback': feedback})
-        session['attempts'] += 1
-    return render_template('index.html', attempts=session['attempts'], feedback=session['feedback'])
+    if request.method == "POST":
+        x = request.form["guess"]
+        x = x.upper()
+        guess = list(x)
+        feedback = mastermind_lib.check_code(session["secret"], guess)
+ 
+        session["feedback"].append({"guess": guess, "feedback": feedback})
+        session["attempts"] += 1
+        attempts = session["attempts"]
+        if feedback == ["X","X","X","X"]:
+            return redirect(url_for('win'))
+        if attempts == 10:
+            return redirect(url_for("loose"))
+    return render_template("index.html", attempts=session["attempts"], feedback=session["feedback"])
 
-if __name__ == '__main__':
+@app.route("/newgame")
+def new_game():
+    session["secret"] = mastermind_lib.code()
+    session["attempts"] = 0
+    session["feedback"] = []
+    return render_template("newgame.html")
+
+@app.route("/win")
+def win():
+    return render_template("win.html", secret=session["secret"])
+@app.route("/loose")
+def loose():
+    return render_template("loose.html", secret=session["secret"])
+if __name__ == "__main__":
     app.run(debug=True)
